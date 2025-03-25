aeb73724c0ed76709c55b0bc72bb2913326483c5
williamGMK <137037723+williamGMK@users.noreply.github.com>
2025-03-25 19:57:43 +0200

    BinarySearch

--- src/Demo.java
+++ src/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
         int  nums[] = {5,7,9,11,13};
         int target = 11;
 
-        int result = linearSearch(nums, target);
+        int result = BinarySearch(nums, target);
 
 
         if(result != -1){
@@ -21,6 +21,28 @@ public class Demo {
                 return i;
             }
         }
+        return -1;
+    }
+
+    public  static int BinarySearch(int[] nums, int target) {
+
+       int left = 0;
+       int right = nums.length-1;
+
+       while (left <= right){
+           int mid = (left + right)/2;
+
+           if(nums[mid] == target){
+               return mid;
+           }  else if(nums[mid] < target){
+               left = mid + 1;
+           }
+           else {
+               right = mid - 1;
+           }
+
+       }
+
         return -1;
     }
 }
