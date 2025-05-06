497eb1c7a4f810665e79de559113f7e1ceade387
williamGMK <137037723+williamGMK@users.noreply.github.com>
2025-05-06 12:31:59 +0200

    BinarySearch

--- src/Demo.java
+++ src/Demo.java
@@ -4,10 +4,12 @@ public class Demo {
         int target = 11;
 
         int result = BinarySearch(nums, target);
+        int result2 = linearSearch(nums, target);
 
 
         if(result != -1){
             System.out.println("Element found at Index : " + result);
+            System.out.println("Element found at Index : " + result2);
         }else {
             System.out.println("Element found at Index");
         }
@@ -15,24 +17,30 @@ public class Demo {
 
 
     public  static int linearSearch(int[] nums, int target) {
-
+        int steps = 0;
         for(int i = 0; i <nums.length; i++){
+            steps++;
             if (nums[i] == target) {
+                System.out.println("Steps taken by Linear : " + steps);
                 return i;
+
             }
         }
+        System.out.println("Steps taken by Linear : " + steps);
         return -1;
     }
 
     public  static int BinarySearch(int[] nums, int target) {
 
+        int steps = 0;
        int left = 0;
        int right = nums.length-1;
 
        while (left <= right){
            int mid = (left + right)/2;
-
+            steps++;
            if(nums[mid] == target){
+               System.out.println("Steps taken by Binary : " + steps);
                return mid;
            }  else if(nums[mid] < target){
                left = mid + 1;
@@ -42,7 +50,7 @@ public class Demo {
            }
 
        }
-
+        System.out.println("Steps taken by Binary : " + steps);
         return -1;
     }
 }
